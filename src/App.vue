9ce1db779304a94a636398973f081214c1b6b313
huna <huna@wemakeprice.com>
2019-06-27 17:52:02 +0900

    Chage the logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img class="logo" src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -19,5 +19,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  width: 200px;
+  height: 200px;
 }
 </style>
